1ee84b095a7d9cdf15005656ea7c4cf00aa62ec2
dmvazli <davidmvazquez1@gmail.com>
2019-05-02 18:55:27 -0400

    filtered last feature element from the query selector

--- js/Feature.js
+++ js/Feature.js
@@ -1,10 +1,7 @@
 class Feature {
     constructor(feature) {
         this.feature = feature;
-        this.featureAbout        = this.feature.querySelector('.feature-about');
-        // this.demoButtonContainer = this.feature.querySelector('.demo-button-container');
-        this.demoButton          = this.feature.querySelector(`.demo-button`);
-
+        this.demoButton = this.feature.querySelector(`.demo-button`);
         this.demoHidden = true;
 
         this.demoButton.addEventListener('click', this.toggleHeight.bind(this));
@@ -24,8 +21,13 @@ class Feature {
 
         this.demoHidden = !this.demoHidden;
     }
+
+    
 }
 
 const features = document.querySelectorAll('.feature');
 
-features.forEach(feature => new Feature(feature));
+features.forEach(feature => {
+    if(feature.dataset.feature != 'grow')
+        new Feature(feature);
+});
